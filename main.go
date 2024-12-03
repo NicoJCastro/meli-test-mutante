2b9db16f8f8c9ca880c29b6423548bd902b9eb06
ncastro@vozy.co <ncastro@vozy.co>
2024-12-03 20:17:46 -0300

    fix loadenv

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
+// func init() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// }
 
 func main() {
 
